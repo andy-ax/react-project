c3beb06dd3a335312dfe21e21a92e2dcce3fe8c7
andy-mars <andy9887776666@gmail.com>
2021-07-19 17:48:00 +0800

    save

--- src/redux/redux-reducer.ts
+++ src/redux/redux-reducer.ts
@@ -1,16 +1,20 @@
-import { createStore, ReducersMapObject, Action, combineReducers, applyMiddleware, Middleware, Store } from 'redux';
+import {
+    createStore,
+    ReducersMapObject,
+    Action,
+    combineReducers,
+    applyMiddleware,
+    Middleware,
+    Store,
+} from "redux";
 
-/**
- * TODO 
- * 1.reducer 添加 
- * 2.middleware 声明
- *  */
 export class ReduxReducer {
-    reducerMap: any = {};
+    reducerMap: ReducersMapObject = {};
     defaultState: any = {};
     store!: Store;
-    middlewareList: Middleware[] =  [];
+    middlewareList: Middleware[] = [];
 
+    // 新增reducer
     addReducer(name: any, reducer: any, defaultState: any) {
         this.reducerMap[name] = (state: any, action: Action) => {
             if (action.type === name) {
@@ -20,12 +24,45 @@ export class ReduxReducer {
         this.defaultState[name] = defaultState;
     }
 
+    // 从已有reducer生成新的reducer
+    /**
+     * reducer复用
+     * @param reducerFunction 需要复用的reducer
+     * @param reducerName reducer命名空间
+     * @param key reducer命名空间在action中的key值
+     * @param reducerFilter reducer筛选,符合条件才会执行reducer
+     * @returns 
+     */
+    createNamedWrapperReducer(reducerFunction: Function, reducerName: string, key: string = 'name', reducerFilter?: Function) {
+        return (state: any, action: Action): void => {
+            const name = (action as any)[key];
+            const isInitializationCall = state === undefined;
+            if (name !== reducerName && !isInitializationCall) {
+                return state;
+            } else if (reducerFilter && !reducerFilter(state, action)) {
+                return state;
+            }
+
+            return reducerFunction(state, action);
+        };
+    }
+
+    // 获取保存的reducer函数
+    getReducerFuc(name: string) {
+        return this.reducerMap[name];
+    }
+
+    // 添加中间件
     addMiddleware(middleware: Middleware) {
-        this.middlewareList.push(middleware)
+        this.middlewareList.push(middleware);
     }
 
     reducerInit() {
         const reducer = combineReducers(this.reducerMap);
-        this.store = createStore(reducer, this.defaultState, applyMiddleware(...this.middlewareList));
+        this.store = createStore(
+            reducer,
+            this.defaultState,
+            applyMiddleware(...this.middlewareList)
+        );
     }
 }
